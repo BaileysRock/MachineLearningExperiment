a56664548fd97188ddaf9ec0026accc779c10042
Baileys <xiong257246@outlook.com>
2021-10-17 16:59:46 +0800

    Experiment 2

--- Experiment2/main.py
+++ Experiment2/main.py
@@ -5,12 +5,12 @@ if __name__ == '__main__':
     # 符合朴素贝叶斯分布的点求解
     # predictResult(0)
     # 不符合朴素贝叶斯分布的点求解
-    # predictResult(1)
+    predictResult(1)
     # 使用梯度下降法对保存的文件 求解
-    predictResult(2,"./DataSet/data_banknote_authentication.csv")
+    # predictResult(2,"./DataSet/data_banknote_authentication.csv")
     # predictResult(2,"./DataSet/sonar.csv")
     # predictResult(2, "./DataSet/heart.csv")
     # 使用牛顿迭代法对保存的文件 求解
-    predictResult(3, "./DataSet/data_banknote_authentication.csv")
+    # predictResult(3, "./DataSet/data_banknote_authentication.csv")
     # predictResult(3, "./DataSet/sonar.csv")
     # predictResult(3, "./DataSet/heart.csv")
